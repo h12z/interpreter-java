1d6e129f0d9f70ad37f4b0dbd570320525a5cb5f
h12z <private@h12z.me>
2024-10-17 07:58:02 +0200

    init

---
+++ app/src/main/java/me/h12z/interpreter/ast/Func.java
@@ -0,0 +1,16 @@
+package me.h12z.interpreter.ast;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Func
+ */
+public class Func {
+
+  HClass parent;
+  List<HType> types;
+  List<Container> containers;
+  List<Action> actions;
+
+}
